72064e8ef2d386e67b22ad833d6d8ccc193615c0
Vanessa Toro <vanetoro84@gmail.com>
2018-04-05 00:58:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
        if (@side_1 == @side_2 ) && (@side_1 == @side_3 ) 
          :equilateral
        elsif
-        @side_1 != @side_2 && @side_2 != @side_3
+        @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
           :scalene
         else
           :isosceles
