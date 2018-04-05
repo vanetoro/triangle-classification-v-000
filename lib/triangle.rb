eb06712e32acc94b0d665514595a2f4916d7bf03
Vanessa Toro <vanetoro84@gmail.com>
2018-04-05 00:41:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,12 @@
 class Triangle
    attr_accessor :side_1, :side_2, :side_3
    
+   def initialize(side_1,side_2, side_3)
+     
+   end
    
+   def kind
+     if side_1 == side_2 && side_2 == side_3
+       :equilateral
+     else 
 end
