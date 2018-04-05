66238c41b13fe707fbe3ae33d4b18455610731a7
Vanessa Toro <vanetoro84@gmail.com>
2018-04-05 01:11:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,7 @@ class Triangle
    
    def kind
        if @side_1 == 0 || @side_2 == 0 || @side_3 == 0 
-           begin
             raise TriangleError
-          rescue TriangleError => error
               puts error.message
           end  
       end
