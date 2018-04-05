aee1c36c748af97ae8ea5f450c371f03c5330d9d
Vanessa Toro <vanetoro84@gmail.com>
2018-04-05 00:44:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
    end
    
    def kind
-     if side_1 == side_2 && side_2 == side_3
+     if side_1 == side_2 && side_2 == side_3 && side_1 == side_3
        :equilateral
      elsif
       side_1 != side_2 && side_2 != side_3 && side_1 != side_3
