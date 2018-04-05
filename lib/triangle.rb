6a15988f60c993eddc89ed720935d3ccd7403261
Vanessa Toro <vanetoro84@gmail.com>
2018-04-05 00:58:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,10 +15,10 @@ class Triangle
               puts error.message
           end  
       end
-       if (@side_1 == @side_2 ) && (@side_1 == @side_3 ) #&& side_1 == side_3
+       if (@side_1 == @side_2 ) && (@side_1 == @side_3 ) 
          :equilateral
        elsif
-        @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != side_3
+        @side_1 != @side_2 && @side_2 != @side_3
           :scalene
         else
           :isosceles
