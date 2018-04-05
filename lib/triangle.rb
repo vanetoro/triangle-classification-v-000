8f02ac9d3c753bcb7a8a0773a25e985c8cc7a5d2
Vanessa Toro <vanetoro84@gmail.com>
2018-04-05 00:55:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,11 @@ class Triangle
    
    def kind
        if @side_1 == 0 || @side_2 == 0 || @side_3 == 0 
-         TriangleError
+           begin
+            raise TriangleError
+          rescue TriangleError => error
+              puts error.message
+          end  
       end
        if (@side_1 == @side_2 ) && (@side_1 == @side_3 ) #&& side_1 == side_3
          :equilateral
